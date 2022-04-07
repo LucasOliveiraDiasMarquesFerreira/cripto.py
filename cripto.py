a7d6ac20f49d490f4c12825fc7ffc82761abc2b2
Lucas Oliveira Dias Marques Ferreira <100947807+TinierClover041@users.noreply.github.com>
2022-04-07 15:52:26 -0300

    Update cripto.py

--- cripto.py
+++ cripto.py
@@ -1,1 +1,19 @@
+alfabeto=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+            "s","t","u","v","w","x","y","z"]
 
+chave=3
+mensagem = str(input("Escreva a mensagem a ser criptografada:"))
+mensagem=mensagem.lower()
+n=len(alfabeto)
+
+cifrada=""
+for letra in mensagem:
+    indice= alfabeto.index(letra)
+    nova_letra= alfabeto[(indice+chave)%n]
+     
+     
+    print(letra)
+    cifrada= cifrada+nova_letra
+    
+print(mensagem)
+print(cifrada)
